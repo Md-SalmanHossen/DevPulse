70efbbb1485891872e9d63c29119d97d67fe36df
SalmanHossen <hossainsalman873@gmail.com>
2026-06-05 11:15:42 +0600

    (fix) : add if not exists to issues table to prevent crash

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { type Application } from 'express';
+import cors from 'cors';
 
 const app : Application = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
 
 
 
--- src/db/index.db.ts
+++ src/db/index.db.ts
@@ -7,6 +7,32 @@ export const pool = new Pool({
 
 export const initDB = async()=>{
    try {
+
+      await pool.query(
+         `CREATE TABLE IF NOT EXISTS users (
+         id SERIAL PRIMARY KEY,
+         name VARCHAR(100) NOT NULL,
+         email VARCHAR(100) UNIQUE NOT NULL,
+         password TEXT NOT NULL,
+         role VARCHAR(20) DEFAULT 'contributor',
+         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+         )`
+      );
+
+      await pool.query(`
+         CREATE TABLE IF NOT EXISTS issues (
+         id SERIAL PRIMARY KEY,
+         title VARCHAR(150) NOT NULL,
+         description TEXT NOT NULL,
+         type VARCHAR(30) NOT NULL,
+         status VARCHAR(30) DEFAULT 'open',
+         reporter_id INTEGER NOT NULL,
+         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+         );
+      `)
+
       console.log("Database connected successfully");
       
    } catch (error) {
